f5986a59565d1a9a0244da97328fbea9becc6b2a
Juan Urtazun <jurtazun@despegar.com>
2015-11-11 20:56:34 -0300

    Add: DI localytics.directives for chosen.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,8 @@
   'use strict';
 
   angular
-    .module('test2015', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('test2015', ['ngAnimate', 'ngCookies', 'ngTouch',
+        'ngSanitize', 'ngMessages', 'ngAria', 'ui.router',
+        'ui.bootstrap', 'toastr', 'localytics.directives']);
 
 })();
