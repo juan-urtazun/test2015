2c5d9531314cdd6371eb35f4f7c017a607ce57db
Juan Urtazun <jurtazun@despegar.com>
2015-11-10 22:21:05 -0300

    Add: app scaffold.

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('test2015', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
