f96b57406e9c7b3b82f474d4a8cdbe6c436d2a1a
Juan Urtazun <jurtazun@despegar.com>
2015-11-11 23:43:52 -0300

    Add: lodash dep.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,6 +4,7 @@
   angular
     .module('test2015', ['ngAnimate', 'ngCookies', 'ngTouch',
         'ngSanitize', 'ngMessages', 'ngAria', 'ui.router',
-        'ui.bootstrap', 'toastr', 'localytics.directives', 'chart.js']);
+        'ui.bootstrap', 'toastr', 'localytics.directives', 'chart.js',
+        'ngLodash']);
 
 })();
